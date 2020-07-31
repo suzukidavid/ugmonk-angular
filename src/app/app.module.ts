e7dc5e71ef415af673cd9dbdd8ce5023b8477d21
code-rain002 <jonny93229@gmail.com>
2020-08-01 04:15:25 +0530

    app module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeroComponent } from './components/hero/hero.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ClothesCardComponent } from './components/clothes-card/clothes-card.component';
+import { ClothesContainerComponent } from './components/clothes-container/clothes-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeroComponent,
+    HomeComponent,
+    ClothesCardComponent,
+    ClothesContainerComponent
   ],
   imports: [
     BrowserModule,
