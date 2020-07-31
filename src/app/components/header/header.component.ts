5ddf60a9963c026e57382b4618ef25afcf410050
code-rain002 <jonny93229@gmail.com>
2020-07-31 19:40:30 +0530

    navbar fix

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  cartIcon = faShoppingCart;
+  userIcon = faUser;
+  
   constructor() { }
 
   ngOnInit(): void {
