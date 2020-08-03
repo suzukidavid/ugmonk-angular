e091a4f2b06fb08fa975e0511deef8c7fef2fc66
code-rain002 <jonny93229@gmail.com>
2020-08-03 19:17:40 +0530

    http modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -9,6 +10,9 @@ import { HeroComponent } from './components/hero/hero.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { ClothesCardComponent } from './components/clothes-card/clothes-card.component';
 import { ClothesContainerComponent } from './components/clothes-container/clothes-container.component';
+import { ClothesByCategoryComponent } from './components/clothes-by-category/clothes-by-category.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +21,17 @@ import { ClothesContainerComponent } from './components/clothes-container/clothe
     HeroComponent,
     HomeComponent,
     ClothesCardComponent,
-    ClothesContainerComponent
+    ClothesContainerComponent,
+    ClothesByCategoryComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
