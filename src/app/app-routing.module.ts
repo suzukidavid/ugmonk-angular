44b0f54e448e0fb69b42c271cd4cb849d1869c1c
code-rain002 <jonny93229@gmail.com>
2020-08-03 19:17:14 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'auth/login', component: LoginComponent},
+  {path: 'auth/register', component: SignupComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
