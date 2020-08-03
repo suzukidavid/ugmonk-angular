a008000416725fc3b4737f257ea4baba719e096a
code-rain002 <jonny93229@gmail.com>
2020-08-03 19:18:14 +0530

    added product model

---
+++ src/app/models/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+    id: number;
+    images: string[];
+    title: string;
+    color: string;
+    desc: string;
+    price: number;
+    quantity: number;
+    short_desc: string;
+    category: string;
+    size: string[];
+    cat_id: number;
+};
