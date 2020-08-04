875c943f2bef318f25d59fb0808c1e6fb4f1f0dd
code-rain002 <jonny93229@gmail.com>
2020-08-05 01:50:47 +0530

    added add to cart option

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { Product } from 'src/app/models/product';
+import { DataSharingService } from '../../services/data-sharing.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   cartIcon = faShoppingCart;
   userIcon = faUser;
-  
-  constructor() { }
+  public cartItems: Product[] = [];
+
+  constructor(private dataSharing:  DataSharingService) {}
 
   ngOnInit(): void {
+    this.getCartItem();
+  }
+
+  public getCartItem(): void {
+    this.dataSharing.cartItem.subscribe(data => {
+        this.cartItems.push(data);
+    });
+  }
+
+  public addCartItems(): void {
+    this.dataSharing.setCartItemsObject(this.cartItems);
   }
 
 }
