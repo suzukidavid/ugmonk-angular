d6a8961cadd7a0470b4790074083700c5960c144
code-rain002 <jonny93229@gmail.com>
2020-08-05 01:51:22 +0530

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,17 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ClothesPageComponent } from './components/pages/clothes-page/clothes-page.component';
 import { SignupComponent } from './components/signup/signup.component';
-
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { ProductPageComponent } from './components/pages/product-page/product-page.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: 'home', component: HomeComponent},
   {path: 'auth/login', component: LoginComponent},
   {path: 'auth/register', component: SignupComponent},
-  {path: 'clothes/:catId', component: ClothesPageComponent}
+  {path: 'clothes/:catId', component: ClothesPageComponent},
+  {path: 'clothes/product/:id', component: ProductPageComponent},
+  {path: 'cart', component: CartPageComponent}
 ];
 
 @NgModule({
