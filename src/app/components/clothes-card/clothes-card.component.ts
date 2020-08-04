da14c91c4f5f8da6f37a98784cd3abbec22deebf
code-rain002 <jonny93229@gmail.com>
2020-08-05 01:49:34 +0530

    clothes card style update

--- src/app/components/clothes-card/clothes-card.component.ts
+++ src/app/components/clothes-card/clothes-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { faRupeeSign } from '@fortawesome/free-solid-svg-icons';
 import { Product } from 'src/app/models/product';
+import { DataSharingService } from 'src/app/services/data-sharing.service';
 
 @Component({
   selector: 'app-clothes-card',
@@ -12,9 +13,12 @@ export class ClothesCardComponent implements OnInit {
   rupeeIcon = faRupeeSign;
   @Input() cloth: Product;
 
-  constructor() { }
+  constructor(private dataSharing:  DataSharingService) { }
 
   ngOnInit(): void {
   }
 
+  public addToCart(): void {
+    this.dataSharing.setCartItem(this.cloth);
+  }
 }
