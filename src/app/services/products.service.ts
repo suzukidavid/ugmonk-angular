3ce9677f2336fc98bcdb6f28e8da1fd157d9b549
code-rain002 <jonny93229@gmail.com>
2020-08-06 15:21:34 +0530

    product service update

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    accept: 'application/json',
+  });
+  
   private baseUrl: string = 'http://localhost:3000';
 
   constructor(private http: HttpClient) { }
@@ -14,6 +19,15 @@ export class ProductsService {
   public getProductByCategoryId(catId: number): Observable<any> {
     return this.http.get(`${this.baseUrl}/products`, {
       params: {cat_id: catId.toString()}
-    })
+    });
   }
+
+  public addItemsToCart(product: Product): Observable<any> {
+    return this.http.post(`${this.baseUrl}/cart`, product, { headers: this.headers });
+  }
+
+  public getItemsFromCart(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/cart`);
+  }
+
 }
