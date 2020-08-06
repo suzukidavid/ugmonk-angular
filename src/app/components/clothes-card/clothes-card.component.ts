d3c85d6a9a14bec5844c02d63b2f97ca4bff9237
code-rain002 <jonny93229@gmail.com>
2020-08-06 15:18:00 +0530

    added ts file

--- src/app/components/clothes-card/clothes-card.component.ts
+++ src/app/components/clothes-card/clothes-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { faRupeeSign } from '@fortawesome/free-solid-svg-icons';
 import { Product } from 'src/app/models/product';
 import { DataSharingService } from 'src/app/services/data-sharing.service';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-clothes-card',
@@ -13,12 +14,13 @@ export class ClothesCardComponent implements OnInit {
   rupeeIcon = faRupeeSign;
   @Input() cloth: Product;
 
-  constructor(private dataSharing:  DataSharingService) { }
+  constructor(private dataSharing:  DataSharingService,private productApi: ProductsService) { }
 
   ngOnInit(): void {
   }
 
   public addToCart(): void {
-    this.dataSharing.setCartItem(this.cloth);
+    this.productApi.addItemsToCart(this.cloth).subscribe(data => console.log(data))
+    // this.dataSharing.setCartItem(this.cloth);
   }
 }
