bcedbda1a6e17ce261c7af318fe2ca850297147f
code-rain002 <jonny93229@gmail.com>
2020-10-06 12:09:11 +0530

    nav bar update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { Product } from 'src/app/models/product';
 import { DataSharingService } from '../../services/data-sharing.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -11,12 +12,14 @@ import { DataSharingService } from '../../services/data-sharing.service';
 export class HeaderComponent implements OnInit {
   cartIcon = faShoppingCart;
   userIcon = faUser;
+  public isLoggedIn: boolean;
   public cartItems: Product[] = [];
 
-  constructor(private dataSharing:  DataSharingService) {}
+  constructor(private auth: AuthService,private dataSharing:  DataSharingService) {}
 
   ngOnInit(): void {
     this.getCartItem();
+    this.auth.currentUser.subscribe(data => this.isLoggedIn = data.isLoggedIn);
   }
 
   public getCartItem(): void {
@@ -27,6 +30,10 @@ export class HeaderComponent implements OnInit {
 
   public addCartItems(): void {
     this.dataSharing.setCartItemsObject(this.cartItems);
+  }
+
+  public logout(): void {
+    this.auth.logout();
   }
 
 }
